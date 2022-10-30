29bc5b18cff83e54f36fae6d6d091217a8584988
Whoosh <nhatkha1407@gmail.com>
2022-10-30 23:00:38 +0700

    Don't tell me you're too blind to see

---
+++ meta-page/src/Controllers/MetaPageController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Jamstackvietnam\MetaPage\Controllers;
+
+use App\Http\Controllers\Controller;
+use Jamstackvietnam\Core\Traits\HasCrudActions;
+use Jamstackvietnam\MetaPage\Models\MetaPage;
+
+class MetaPageController extends Controller
+{
+    use HasCrudActions;
+
+    public $model = MetaPage::class;
+
+    public function getData()
+    {
+        $metaPages = MetaPage::getAll();
+
+        return ['data' => $metaPages];
+    }
+
+    public function form()
+    {
+        $url = request()->input('id') ?? '';
+        $item = MetaPage::where('url', $url)->firstOrFail();
+
+        return Inertia::render($this->folder() . '/Form', [
+            'item' => $item,
+            'rules' => $this->getModelRules(__FUNCTION__)
+        ]);
+    }
+
+    public function filter($query)
+    {
+        request()->merge(['paginate' => false]);
+        return $query;
+    }
+}
