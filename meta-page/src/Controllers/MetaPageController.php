5b2b0f016707468edf7b0da1b3a58074bbe31ab8
Whoosh <nhatkha1407@gmail.com>
2022-10-30 23:25:38 +0700

    If it's hacky and you know it clap you hands (clap clap)!

--- meta-page/src/Controllers/MetaPageController.php
+++ meta-page/src/Controllers/MetaPageController.php
@@ -2,6 +2,7 @@
 
 namespace Jamstackvietnam\MetaPage\Controllers;
 
+use Inertia\Inertia;
 use App\Http\Controllers\Controller;
 use Jamstackvietnam\Core\Traits\HasCrudActions;
 use Jamstackvietnam\MetaPage\Models\MetaPage;
@@ -24,9 +25,19 @@ class MetaPageController extends Controller
         $url = request()->input('id') ?? '';
         $item = MetaPage::where('url', $url)->firstOrFail();
 
+        $breadcrumbs = [[
+            'url' => route($this->getResource() . '.index'),
+            'name' => trans('models.table_list.' . $this->getTable()),
+        ]];
+
+        if (request()->wantsJson()) {
+            return response()->json($item);
+        }
+
         return Inertia::render($this->folder() . '/Form', [
             'item' => $item,
-            'rules' => $this->getModelRules(__FUNCTION__)
+            'breadcrumbs' => $breadcrumbs,
+            'schema' => $this->getSchema(),
         ]);
     }
 
