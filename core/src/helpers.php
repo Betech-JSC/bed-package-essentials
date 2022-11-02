db9770c8e7c546d3e56323ce20589c28bd822a03
Whoosh <nhatkha1407@gmail.com>
2022-11-02 21:21:15 +0700

    removing unit tests

---
+++ core/src/helpers.php
@@ -0,0 +1,22 @@
+<?php
+
+if (! function_exists('setMeta')) {
+    function setMeta()
+    {
+        $seo = settings()->group('general')->all();
+
+        $title = $seo->seo_meta_title;
+        $description = $seo->seo_meta_description;
+        $canonical = $seo->seo_canonical;
+        $robots = $seo->seo_meta_robots;
+        $keywords = $seo->seo_meta_keywords;
+        $image = $seo->seo_image;
+
+        SEO::setTitle($title);
+        SEO::setDescription($description);
+        SEO::setCanonical($canonical);
+        SEO::setRobots($robots);
+        SEO::setKeywords($keywords);
+        SEO::opengraph()->addImage($image);
+    }
+}
