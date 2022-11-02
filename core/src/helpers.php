1fcb13b048f772329c327e43da37c82608f85e13
Whoosh <nhatkha1407@gmail.com>
2022-11-02 21:52:36 +0700

    various changes

--- core/src/helpers.php
+++ core/src/helpers.php
@@ -1,16 +1,18 @@
 <?php
 
-if (! function_exists('setupSeo')) {
+if (!function_exists('setupSeo')) {
     function setupSeo()
     {
         $seo = settings()->group('general')->all();
 
+        $site = $seo['seo_site_name'] ?: null;
         $title = $seo['seo_meta_title'] ?: null;
         $description = $seo['seo_meta_description'] ?: null;
         $canonical = $seo['seo_canonical'] ?: null;
         $robots = $seo['seo_meta_robots'] ?: null;
         $keywords = $seo['seo_meta_keywords'] ?: null;
         $image = $seo['seo_image'] ?: null;
+        $url = url()->current();
 
         SEO::setTitle($title);
         SEO::setDescription($description);
@@ -22,5 +24,15 @@ if (! function_exists('setupSeo')) {
         SEO::metatags()->setKeywords($keywords);
 
         SEO::opengraph()->addImage($image);
+        SEO::opengraph()->setUrl($url);
+        SEO::opengraph()->setSiteName($site);
+
+        SEO::twitter()->setSite($site);
+
+        SEO::jsonLd()->setTitle($type);
+        SEO::jsonLd()->setSite($type);
+        SEO::jsonLd()->setDescription($description);
+        SEO::jsonLd()->setUrl($url);
+        SEO::jsonLd()->setImage($image);
     }
 }
