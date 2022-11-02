362aa6ca302028814f83d4f2f6cb9ab2721ca35b
Whoosh <nhatkha1407@gmail.com>
2022-11-02 21:34:31 +0700

    fixes

--- core/src/helpers.php
+++ core/src/helpers.php
@@ -3,14 +3,14 @@
 if (! function_exists('setupSeo')) {
     function setupSeo()
     {
-        $seo = settings()->group('general')->all();
+        $seo = collect(settings()->group('general')->all());
 
-        $title = $seo->seo_meta_title;
-        $description = $seo->seo_meta_description;
-        $canonical = $seo->seo_canonical;
-        $robots = $seo->seo_meta_robots;
-        $keywords = $seo->seo_meta_keywords;
-        $image = $seo->seo_image;
+        $title = $seo?->seo_meta_title;
+        $description = $seo?->seo_meta_description;
+        $canonical = $seo?->seo_canonical;
+        $robots = $seo?->seo_meta_robots;
+        $keywords = $seo?->seo_meta_keywords;
+        $image = $seo?->seo_image;
 
         SEO::setTitle($title);
         SEO::setDescription($description);
