52a1d873e2ff725420d422c3e9a323bf2b687020
Whoosh <nhatkha1407@gmail.com>
2022-11-04 00:55:09 +0700

    Nitpicking about alphabetizing methods, minor OCD thing

--- core/src/Providers/MacroServiceProvider.php
+++ core/src/Providers/MacroServiceProvider.php
@@ -24,7 +24,7 @@ class MacroServiceProvider extends BaseServiceProvider
     private function registerMacroBlueprint()
     {
         Blueprint::macro('addTimestamps', function () {
-            $this->unsignedBigInteger('created_by')->index();
+            $this->unsignedBigInteger('created_by')->nullable()->index();
             $this->unsignedBigInteger('updated_by')->nullable()->index();
             $this->unsignedBigInteger('deleted_by')->nullable()->index();
 
