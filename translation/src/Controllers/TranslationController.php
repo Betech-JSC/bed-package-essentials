6a60cd8408f9cad1aff353141cca53fee701bf71
Whoosh <nhatkha1407@gmail.com>
2022-11-06 19:57:23 +0700

    Update TranslationController.php

--- translation/src/Controllers/TranslationController.php
+++ translation/src/Controllers/TranslationController.php
@@ -11,32 +11,4 @@ class TranslationController extends Controller
     use HasCrudActions;
 
     public $model = Translation::class;
-
-    public function beforeIndex($query)
-    {
-        $pages = Translation::all();
-
-        $storedRoutes = $pages->pluck('url');
-        $routes = collect(Sitemap::create()->addStaticRoutes()->tags)
-            ->transform(
-                fn ($item) =>
-                str_replace(env('APP_URL'), '', $item['url']) ?: '/'
-            );
-
-        $diff = $routes->diff($storedRoutes);
-
-        if ($diff->count()) {
-            Translation::insert($diff->transform(fn ($item) => ['url' => $item])->toArray());
-        }
-
-        return $query->orderBy('url', 'ASC');
-    }
-
-    private function transform($item)
-    {
-        return [
-            ...$item->toArray(),
-            'url' => env('APP_URL') . $item['url']
-        ];
-    }
 }
