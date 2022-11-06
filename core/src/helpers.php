586f809c85cb98ad8e3a175a4b04ab40527901a7
Whoosh <nhatkha1407@gmail.com>
2022-11-06 18:28:46 +0700

    fix helper

--- core/src/helpers.php
+++ core/src/helpers.php
@@ -1,12 +1,1 @@
 <?php
-
-if (!function_exists('lang_path_with_prefix')) {
-    /**
-     * @param string $path
-     * @return string
-     */
-    function lang_path_with_prefix($path = '', $prefix = 'Frontend/')
-    {
-        return resource_path($prefix . 'lang' . ($path !== '' ? DIRECTORY_SEPARATOR . $path : ''));
-    }
-}
--- localizator/src/helpers.php
+++ localizator/src/helpers.php
@@ -1,12 +1,12 @@
 <?php
 
-if (! function_exists('lang_path')) {
+if (!function_exists('lang_path')) {
     /**
      * @param string $path
      * @return string
      */
-    function lang_path($path = '')
+    function lang_path($path = '', $prefix = 'Frontend/')
     {
-        return resource_path('lang'.($path !== '' ? DIRECTORY_SEPARATOR.$path : ''));
+        return resource_path($prefix . 'lang' . ($path !== '' ? DIRECTORY_SEPARATOR . $path : ''));
     }
 }
