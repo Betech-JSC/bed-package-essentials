242d6b58d8fdac5e8adb285ac19b7f8dbe375da1
LinhPhan <phanlinh9605@gmail.com>
2022-11-08 16:12:21 +0700

    Fix translation

--- core/src/Models/Translatable.php
+++ core/src/Models/Translatable.php
@@ -3,6 +3,7 @@
 namespace Jamstackvietnam\Core\Models;
 
 use Astrotomic\Translatable\Translatable as AstrotomicTranslatable;
+use Illuminate\Database\Eloquent\Builder;
 
 trait Translatable
 {
@@ -10,7 +11,33 @@ trait Translatable
 
     public function getDefaultLocale(): ?string
     {
-        return session()->get('locale', config('app.locale'));
+        return current_locale();
+    }
+
+    public function getTranslationStatusesAttribute()
+    {
+        $translations = [];
+        foreach (config('app.locales') as $locale) {
+            $translations[$locale] = $this->hasTranslation($locale);
+        }
+        return $translations;
+    }
+
+    public function scopeActive($query)
+    {
+        $query = $query->where('status', 'ACTIVE');
+
+        if (!empty(request()->route()->getName())) {
+            $locale = current_locale();
+            $default_locale = config('app.locale');
+
+            $query = $query->whereHas('translations', function (Builder $q) use ($locale, $default_locale) {
+                $q->where('locale', '=', $locale)
+                    ->orWhere('locale', '=', $default_locale);
+            });
+        }
+
+        return $query;
     }
 
     public function scopeWhereSlug($query, $slug)
