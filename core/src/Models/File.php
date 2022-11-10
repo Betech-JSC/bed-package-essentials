66211e4fc9215d3838f0190de254c65323d05467
Whoosh <nhatkha1407@gmail.com>
2022-11-10 09:23:15 +0700

    Update File.php

--- core/src/Models/File.php
+++ core/src/Models/File.php
@@ -2,158 +2,272 @@
 
 namespace JamstackVietnam\Core\Models;
 
-use App\Models\Admin;
-use Illuminate\Support\Facades\Storage;
-use JamstackVietnam\Core\Models\BaseModel;
-use Nicolaslopezj\Searchable\SearchableTrait;
+use Image;
 use Illuminate\Support\Str;
-use Illuminate\Support\Facades\File as FileFacades;
+use RecursiveIteratorIterator;
+use RecursiveDirectoryIterator;
+use Illuminate\Core\Facades\Storage;
 
-class File extends BaseModel
+class File
 {
-    use SearchableTrait;
+    protected $path;
+    protected $disk;
+    protected $storage;
+    protected $contents;
 
-    public $fillable = [
-        'disk',
-        'path',
-        'filename',
-        'extension',
-        'mime',
-        'size',
-        'width',
-        'height',
-        'creator_id',
-        'creator_type',
-        'editor_id',
-        'editor_type',
-        'created_at',
-        'updated_at',
+    public const MAX_SIZE_LIST = [
+        'image' => 5,
+        'video' => 50,
+        'application' => 100,
+        'others' => 10,
     ];
 
-    public $deploy = false;
-
-    protected $hidden = [];
-
-    protected $appends = ['image_id', 'image_url', 'static_url'];
-
-    protected $searchable = [
-        'columns' => [
-            'files.filename' => 10,
-        ],
-    ];
-
-    protected static function booted()
+    public function __construct($path = '/', $disk = null)
     {
-        static::deleted(function ($file) {
-            $path = $file->getRawOriginal('path');
-            Storage::disk($file->disk)->delete($path);
-            Storage::deleteDirectory($file->disk . '/cache/' . $path);
-        });
+        $this->path = $path;
+        $this->disk = $disk ?? 'uploads';
+        $this->storage = Storage::disk($this->disk);
     }
 
-    public function getImageIdAttribute()
+    public function items()
     {
-        return $this->id;
+        $this->contents = collect($this->storage->listContents($this->path));
+
+        $tree = $this->tree();
+        $directories = $this->directories();
+        $files = $this->files();
+
+        return compact('tree', 'directories', 'files');
     }
 
-    public function getImageUrlAttribute()
+    public function tree()
     {
-        if (!is_null($this->path)) {
-            return $this->id . '/' . $this->path;
+        $rootPath = $this->storage->path('/');
+        $flatItems = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($rootPath),
+            RecursiveIteratorIterator::CHILD_FIRST
+        );
+
+        $tree = [];
+        foreach ($flatItems as $item) {
+            if (
+                !$item->isDir() ||
+                $this->firstCharIs($item->getFilename(), '.')
+            ) continue;
+
+            $path = [$item->getFilename() => []];
+
+            for ($depth = $flatItems->getDepth() - 1; $depth >= 0; $depth--) {
+                $path = [$flatItems->getSubIterator($depth)->current()->getFilename() => $path];
+            }
+            $tree = array_merge_recursive($tree, $path);
         }
-        return null;
+
+        return $this->transformTree($tree);
     }
 
-    public function getStaticUrlAttribute()
+    public function transformTree($item, $name = null, $path = null)
     {
-        if (!is_null($this->image_url)) {
-            return static_url($this->image_url);
+        $itemChildren = collect($item)
+            ->map(fn ($subItem, $subKey) => $this->transformTree($subItem, $subKey, implode('/', [$path, $subKey])))
+            ->filter()
+            ->sortBy('path')
+            ->keyBy('path')
+            ->values()
+            ->toArray();
+
+        if (is_null($path)) {
+            return [[
+                'slug' => Str::slug('/') .  '-' . generate_code(5),
+                'name' => 'File Manager',
+                'path' => '/',
+                'children' => $itemChildren,
+            ]];
         }
-        return null;
-    }
 
-    public function getAbsoluteUrlAttribute()
-    {
-        if (!is_null($this->image_url)) {
-            return Storage::disk($this->disk)->url($this->image_url);
-        }
-        return null;
-    }
-
-    public static function storeMedia($file, $uploader)
-    {
-        $filename = $file->getClientOriginalName();
-        $path = Storage::disk('uploads')->putFileAs(date('Y/m/d'), $file, $filename);
-
-        $media = [
-            'disk' => 'uploads',
+        return [
+            'slug' => Str::slug($path) .  '-' . generate_code(5),
+            'name' => $name,
             'path' => $path,
-            'filename' => $filename,
-            'extension' => $file->guessClientExtension(),
-            'mime' => $file->getClientMimeType(),
-            'size' => $file->getSize(),
-            'creator_id' => $uploader->id,
-            'creator_type' => get_class($uploader),
-            'editor_id' => $uploader->id,
-            'editor_type' => get_class($uploader)
+            'children' => $itemChildren,
         ];
-
-        if (
-            substr($file->getMimeType(), 0, 5) == 'image' &&
-            $media['extension'] !== 'svg'
-        ) {
-            $imageSize = getimagesize($file);
-            $media['width'] = $imageSize[0];
-            $media['height'] = $imageSize[1];
-        }
-
-        return self::create($media);
     }
 
-    public static function storeMediaFromUrl($url, $uploader = null, $filename = null)
+    public function directories()
+    {
+        return $this->contents
+            ->filter(fn ($item) => $item->isDir())
+            ->values()
+            ->toArray();
+    }
+
+    public function files()
+    {
+        return $this->contents
+            ->filter(fn ($item) => $item->isFile())
+            ->values()
+            ->map(fn ($item) => $this->transformFile($item))
+            ->reject(fn ($item) => $this->firstCharIs($item['filename'], '.'))
+            ->sortBy('path')
+            ->keyBy('path');
+    }
+
+    public function findOrFail($options = [])
+    {
+        $filePath = $this->storage->get($this->path);
+        $mimeType = $this->storage->mimeType($this->path);
+
+        if (contains($mimeType, 'image/') && $mimeType !== 'image/heic') {
+            if (isset($options['cache'])) {
+                $image = Image::cache(function ($image) use ($filePath, $options) {
+                    $image->make($filePath);
+                    if ($width = $options['w']) {
+                        $image->resize($width, null, function ($constraint) {
+                            $constraint->aspectRatio();
+                        });
+                    }
+                });
+                return response()->make(
+                    $image,
+                    200,
+                    ['Content-Type' => $mimeType]
+                );
+            }
+
+            $image = Image::make($filePath);
+
+            $format = $options['fm'] ?? 'webp';
+
+            if (isset($options['w'])) {
+                $image->resize($options['w'], null, function ($constraint) {
+                    $constraint->aspectRatio();
+                });
+            }
+
+            return $image
+                ->encode($format)
+                ->response();
+        }
+
+        return response()->make(
+            $filePath,
+            200,
+            ['Content-Type' => $mimeType]
+        );
+    }
+
+    public function store($files)
+    {
+        $successFiles = [];
+        $failureFiles = [];
+
+        foreach ($files as $file) {
+            if ($this->fileValidation($file)) {
+                $successFiles[] = $this->storage->putFileAs(
+                    $this->path,
+                    $file,
+                    $file->getClientOriginalName()
+                );
+            } else {
+                $failureFiles[] = $file->getClientOriginalName();
+            }
+        }
+        return [
+            'successFiles' => $successFiles,
+            'failureFiles' => $failureFiles,
+        ];
+    }
+
+    public function storeFromUrl($url)
     {
         try {
-            $uploader = $uploader ? $uploader : Admin::first();
-
             $file = file_get_contents($url);
             $mime = (new \finfo(FILEINFO_MIME_TYPE))->buffer($file);
+
             $extension = explode('/', $mime)[1] ?? 'png';
             if (!in_array($extension, ['png', 'jpeg', 'jpg', 'webp', 'gif', 'tiff'])) {
-                logger('-------------------');
-                logger($url);
-                logger('-------------------');
-                return;
-            }
-            $filename = Str::slug($filename ?? urldecode(pathinfo($url)['filename'])) . '.' . $extension;
-            $path = date('Y/m/d') . "/$filename";
-            $fullpath = config('filesystems.disks.uploads.root') .  "/" . $path;
-
-            $dirname = pathinfo($fullpath)['dirname'];
-            if (!FileFacades::isDirectory($dirname)) {
-                FileFacades::makeDirectory($dirname, 0777, true, true);
+                logger()->error('Can not store image: ' . $url);
+                return false;
             }
 
-            file_put_contents($fullpath, $file);
+            $filename = Str::slug(urldecode(pathinfo($url)['filename'])) . '.' . $extension;
 
-            $media = [
-                'disk' => 'uploads',
-                'path' => $path,
-                'filename' => $filename,
-                'extension' => $extension,
-                'mime' => $mime,
-                'size' => null,
-                'creator_id' => $uploader->id,
-                'creator_type' => get_class($uploader),
-                'editor_id' => $uploader->id,
-                'editor_type' => get_class($uploader)
-            ];
+            $this->storage->put($filename, $file);
+            return parse_url($this->storage->url($filename))['path'];
 
-            return static::create($media);
-        } catch (\Exception $e) {
-            logger('-------- storeMediaFromUrl --------');
-            logger($url);
-            throw $e;
-            return null;
+        } catch (\Throwable $th) {
+            logger()->error('Can not store image: ' . $url);
+            logger()->error($th->getMessage());
+            return false;
         }
+    }
+
+    public function delete($items)
+    {
+        $deletedItems = [];
+
+        foreach ($items as $item) {
+            if (!$this->storage->exists($item['path'])) {
+                continue;
+            } else {
+                if ($item['type'] === 'dir') {
+                    $this->storage->deleteDirectory($item['path']);
+                } else {
+                    $this->storage->delete($item['path']);
+                }
+            }
+
+            $deletedItems[] = $item;
+        }
+
+        return $deletedItems;
+    }
+
+    public function folderCreate($name)
+    {
+        if ($this->storage->exists($name)) {
+            return false;
+        }
+
+        return (bool) $this->storage->makeDirectory($name);
+    }
+
+    private function formatBytes($size)
+    {
+        $base = log($size) / log(1024);
+        $suffix = array("bytes", "KB", "MB", "GB", "TB")[floor($base)];
+        return round(pow(1024, $base - floor($base)), 2) .  ' ' . $suffix;
+    }
+
+    private function firstCharIs($string, $char)
+    {
+        return mb_substr($string, 0, 1) === $char;
+    }
+
+    private function fileValidation($file)
+    {
+        $mimeType = $file->getMimeType();
+        $maxSize = self::MAX_SIZE_LIST['others'];
+        foreach (self::MAX_SIZE_LIST as $key => $size) {
+            if (contains($mimeType, $key)) {
+                $maxSize = $size;
+            }
+        }
+
+        return $file->getSize() / 1024 / 1024 <= $maxSize;
+    }
+
+    private function transformFile($item)
+    {
+        $metadata = $item->jsonSerialize();
+        $filename = basename($metadata['path']);
+
+        return array_merge($metadata, [
+            'search_name' => str_replace('-', ' ', Str::slug($filename)),
+            'filename' => $filename,
+            'extension' => pathinfo($metadata['path'], PATHINFO_EXTENSION),
+            'static_url' => $this->storage->url($metadata['path']),
+            'formatted_file_size' => $this->formatBytes($metadata['file_size']),
+        ]);
     }
 }
