f64d9eb977145df50cea5e47ee190bea92553def
Whoosh <nhatkha1407@gmail.com>
2022-11-14 22:43:29 +0700

    I honestly wish I could remember what was going on here...

--- core/src/Resources/Backend/js/Pages/FileManager.vue
+++ core/src/Resources/Backend/js/Pages/FileManager.vue
@@ -1,16 +1,12 @@
 <template>
-  <Authenticated>
-    <FileManager :show="true" :selectable="false" />
-  </Authenticated>
+  <FileManager :show="true" :selectable="false" />
 </template>
 
 <script>
-import Authenticated from '@Core/Layouts/Authenticatedl.vue';
-import FileManager from '@Core/Components/FileManager.vue';
+import FileManager from "@Core/Components/FileManager.vue";
 
 export default {
   components: {
-    Authenticated,
     FileManager,
   },
 };
