687d380859c2a31b7e2c8f7a0f3ae26a4aa2df96
Whoosh <nhatkha1407@gmail.com>
2022-11-17 18:35:02 +0700

    It's possible! you can turn a 50-line code chunk into just 3 lines. Here's how

--- core/resources/Backend/js/Pages/Admins/Form.vue
+++ core/resources/Backend/js/Pages/Admins/Form.vue
@@ -41,7 +41,7 @@
                         keyBy: 'name',
                         labelBy: 'title',
                         source: {
-                            model: 'JamstackVietnam\Core\Models\Role',
+                            model: 'JamstackVietnam\\Core\\Models\\Role',
                             method: 'getRoles',
                             only: ['name', 'title'],
                         },
