6a11790e461c98edfd632c705dd99a50cd5a8b8b
LinhPhan9605 <71279918+LinhPhan9605@users.noreply.github.com>
2022-12-15 12:19:02 +0700

    fix

--- blog/src/Models/Post.php
+++ blog/src/Models/Post.php
@@ -181,8 +181,7 @@ class Post extends BaseModel
 
     public function scopeActive($query)
     {
-        return $query
-            ->where('status', self::STATUS_ACTIVE)
+        return $query->where('status', self::STATUS_ACTIVE)
             ->where('published_at', '<=', now());
     }
 
