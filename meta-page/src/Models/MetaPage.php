6235396f06c3ca31bb06043e655965ddea347558
LinhPhan9605 <71279918+LinhPhan9605@users.noreply.github.com>
2023-01-05 00:28:37 +0700

    fix

--- meta-page/src/Models/MetaPage.php
+++ meta-page/src/Models/MetaPage.php
@@ -34,17 +34,6 @@ class MetaPage extends Model
             'seo_meta_title' => $this->seo_meta_title,
             'seo_slug' => $this->seo_slug,
             'seo_meta_description' => $this->seo_meta_description,
-        ];
-    }
-
-    public function transformDetails()
-    {
-        return [
-            'id' => $this->id,
-            'url' => $this->url,
-            'seo_meta_title' => $this->seo_meta_title,
-            'seo_slug' => $this->seo_slug,
-            'seo_meta_description' => $this->seo_meta_description,
             'seo_meta_keywords' => $this->seo_meta_keywords,
             'seo_meta_robots' => $this->seo_meta_robots,
             'seo_canonical' => $this->seo_canonical,
@@ -55,5 +44,10 @@ class MetaPage extends Model
             'inject_body_start' => $this->inject_body_start,
             'inject_body_end' => $this->inject_body_end
         ];
+    }
+
+    public function transformDetails()
+    {
+        return $this->transform();
     }
 }
