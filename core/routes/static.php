c2dd1052972e44d49f29ea5a72952115454a7d2b
Whoosh <nhatkha1407@gmail.com>
2023-01-05 18:30:16 +0700

    I was told to leave it alone, but I have this thing called OCD, you see

--- core/routes/static.php
+++ core/routes/static.php
@@ -4,9 +4,15 @@ use Illuminate\Support\Facades\Route;
 use JamstackVietnam\Core\Controllers\FileController;
 
 if (config('app.url') !== config('app.frontend_url')) {
-    Route::domain(config('app.static_url'))->get('{path?}', [FileController::class, 'show'])
-        ->where('path', '(.*)')->name('files.show');
+    Route::get('static/{path?}', [FileController::class, 'show'])
+        ->where('path', '(.*)');
+
+    Route::domain(config('app.static_url'))
+        ->get('{path?}', [FileController::class, 'show'])
+        ->where('path', '(.*)')
+        ->name('files.show');
 } else {
     Route::get('static/{path?}', [FileController::class, 'show'])
-        ->where('path', '(.*)')->name('files.show');
+        ->where('path', '(.*)')
+        ->name('files.show');
 }
