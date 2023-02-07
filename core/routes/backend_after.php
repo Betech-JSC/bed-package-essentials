1320b9125ca465244a4ea76558046694cdf4235a
LinhPhan9605 <71279918+LinhPhan9605@users.noreply.github.com>
2023-02-07 19:08:12 +0700

    fix setting bar

--- core/routes/backend_after.php
+++ core/routes/backend_after.php
@@ -42,7 +42,6 @@ Route::middleware(['auth:admin'])->name('admin.')->group(function () {
 
     Route::module(FileController::class, ['only' => ['index', 'form', 'store', 'destroy']]);
     Route::post('folders/create', [FileController::class, 'folderCreate'])->name('files.folders.create');
-    Route::post('folders/delete', [FileController::class, 'folderDelete'])->name('files.folders.delete');
 
     Route::post('model-data', [HelperController::class, 'getModelData'])->name('helper.model-data');
 
