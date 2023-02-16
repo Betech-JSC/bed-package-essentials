a46296442b12589fd00f7e36bd30e91fa372d487
Whoosh <nhatkha1407@gmail.com>
2023-02-16 17:00:14 +0700

    Update Table.vue

--- core/resources/Backend/js/Components/Table.vue
+++ core/resources/Backend/js/Components/Table.vue
@@ -4,7 +4,7 @@
         :lazy="true"
         :paginator="true"
         :rows="rows"
-        v-model:filters="filters"
+        v-model:filters="lazyParams.filters"
         ref="data-table"
         dataKey="id"
         :totalRecords="totalItems"
@@ -28,12 +28,7 @@
             <div class="space-y-2">
                 <div class="flex items-center justify-between">
                     <div class="flex items-center space-x-2">
-                        <Link
-                            v-if="canCreate"
-                            class="p-button btn-primary"
-                            text="Thêm mới"
-                            :href="formUrl"
-                        />
+                        <Link v-if="canCreate" class="p-button btn-primary" text="Thêm mới" :href="formUrl" />
                         <a
                             v-if="canExport"
                             class="p-button btn-outline-primary"
@@ -62,15 +57,14 @@
                         </div>
                     </span>
                     <div class="ml-auto mr-2" v-if="$slots.filter">
-                        <slot name="filter" :filters="filters"></slot>
+                        <slot name="filter" :filters="lazyParams.filters"></slot>
                     </div>
                     <div class="p-input-icon-left w-[20rem]">
-                        <heroicons-outline:search
-                            class="absolute transform -translate-y-1/2 top-1/2 left-2"
-                        />
+                        <heroicons-outline:search class="absolute transform -translate-y-1/2 top-1/2 left-2" />
                         <InputText
-                            v-model="filters['global'].value"
+                            v-model="lazyParams.filters['global'].value"
                             placeholder="Tìm kiếm.."
+                            @change="setNewParams"
                         />
                     </div>
                 </div>
@@ -79,15 +73,8 @@
         <template #empty> Không tìm thấy dữ liệu. </template>
         <template #loading>Đang tải dữ liệu...</template>
 
-        <Column
-            selectionMode="multiple"
-            headerStyle="width: 3rem"
-            v-if="showCheckbox"
-        ></Column>
-        <template
-            v-if="displayColumns"
-            v-for="(column, index) in displayColumns"
-        >
+        <Column selectionMode="multiple" headerStyle="width: 3rem" v-if="showCheckbox"></Column>
+        <template v-if="displayColumns" v-for="(column, index) in displayColumns">
             <Column
                 :field="column.field"
                 filterMatchMode="contains"
@@ -101,7 +88,7 @@
                         width="80"
                         preview
                     />
-                    <Link
+                    <!-- <Link
                         v-else
                         :href="
                             route(`admin.${currentResource}.form`, {
@@ -116,17 +103,30 @@
                         >
                             {{ transformCell(data, column) }}
                         </span>
-                        <span v-else v-html="transformCell(data, column)">
+                        <span v-else v-html="transformCell(data, column)"> </span>
+                    </Link> -->
+                    <a
+                        v-else
+                        :href="
+                            route(`admin.${currentResource}.form`, {
+                                id: data.id,
+                            })
+                        "
+                    >
+                        <span
+                            v-if="getStyles(data, column)"
+                            class="px-2 py-1 text-xs font-medium uppercase whitespace-pre rounded-sm"
+                            :style="getStyles(data, column)"
+                        >
+                            {{ transformCell(data, column) }}
                         </span>
-                    </Link>
+                        <span v-else v-html="transformCell(data, column)"> </span>
+                    </a>
                 </template>
-                <template
-                    #filter="{ filterCallback }"
-                    v-if="filters[column.field]"
-                >
+                <template #filter="{ filterCallback }" v-if="lazyParams.filters[column.field]">
                     <InputText
                         type="text"
-                        v-model="filters[column.fields].value"
+                        v-model="lazyParams.filters[column.fields].value"
                         @keydown.enter="filterCallback()"
                         placeholder="Tìm kiếm"
                     />
@@ -137,7 +137,8 @@
 </template>
 
 <script>
-import { FilterMatchMode } from "primevue/api";
+import cloneDeep from 'lodash.clonedeep'
+import { FilterMatchMode } from 'primevue/api'
 export default {
     props: {
         schema: {
@@ -158,162 +159,111 @@ export default {
             rows: 20,
             totalItems: 0,
             loading: false,
-            lazyParams: {},
             timer: null,
 
             selectedItems: null,
             selectAll: false,
 
             mergedColumns: this.mergeColumns(),
-            filters: this.getFilters(),
             filter_begin_time: null,
             filter_end_time: null,
-        };
+
+            lazyParams: {
+                page: 1,
+                // rows: this.$refs['data-table'].rows,
+                rows: 20,
+                sortField: null,
+                sortOrder: null,
+                filters: this.getFilters(),
+            },
+        }
     },
     computed: {
         currentResource() {
-            return this.config.resource ?? this.getResource();
+            return this.config.resource ?? this.getResource()
         },
         tableName() {
-            return (
-                this.config.name ??
-                this.tt("models.table_list." + this.currentResource)
-            );
+            return this.config.name ?? this.tt('models.table_list.' + this.currentResource)
         },
         hideHeader() {
-            return this.config.hideHeader === true || false;
+            return this.config.hideHeader === true || false
         },
         showCheckbox() {
-            return this.config.showCheckbox ?? false;
+            return this.config.showCheckbox ?? false
         },
         showFilter() {
-            return this.config.showFilter ?? false;
+            return this.config.showFilter ?? false
         },
         formUrl() {
-            return (
-                this.config.formUrl ??
-                this.route(`admin.${this.currentResource}.form`)
-            );
+            return this.config.formUrl ?? this.route(`admin.${this.currentResource}.form`)
         },
         sortByDate() {
-            return this.config.sortByDate ?? false;
+            return this.config.sortByDate ?? false
         },
         displayColumns() {
             return Object.values(this.mergedColumns)
                 .filter((x) => x.display)
-                .filter(
-                    (x) =>
-                        x.field !== "slug" &&
-                        x.field !== "locale" &&
-                        !x.field.includes("seo_")
-                )
+                .filter((x) => x.field !== 'slug' && x.field !== 'locale' && !x.field.includes('seo_'))
                 .sort((a, b) => (a.order > b.order ? 1 : -1))
-                .slice(0, 8);
+                .slice(0, 8)
         },
         canCreate() {
-            return (
-                this.config.canCreate ??
-                this.can("admin." + this.currentResource + ".form")
-            );
+            return this.config.canCreate ?? this.can('admin.' + this.currentResource + '.form')
         },
         canExport() {
-            let routeName =
-                this.getCurrentLocale() +
-                ".admin." +
-                this.currentResource +
-                ".export";
+            let routeName = this.getCurrentLocale() + '.admin.' + this.currentResource + '.export'
             return (
                 Object.keys(this.route().t.routes).includes(routeName) &&
-                (this.config.canExport ??
-                    this.can("admin." + this.currentResource + ".export"))
-            );
+                (this.config.canExport ?? this.can('admin.' + this.currentResource + '.export'))
+            )
         },
     },
     watch: {
         selectedItems(value) {
-            this.$emit("on-select", value);
+            this.$emit('on-select', value)
         },
-        filters: {
+        lazyParams: {
             handler() {
-                this.pushToUrl();
+                this.pushToUrl()
             },
             deep: true,
         },
     },
     mounted() {
-        this.loading = true;
-
-        this.lazyParams = {
-            page: 0,
-            rows: this.$refs["data-table"].rows,
-            sortField: null,
-            sortOrder: null,
-            filters: this.getFilters(),
-        };
-
-        this.loadLazyData();
+        this.loadLazyData()
     },
     methods: {
         pushToUrl() {
-            let params = {
-                ...route().params,
-                ...this.filters,
-            };
-            const newUrl = this.route(
-                `admin.${this.currentResource}.index`,
-                params
-            );
-            history.pushState({}, null, newUrl);
-
             if (this.timer) {
-                clearTimeout(this.timer);
-                this.timer = null;
+                clearTimeout(this.timer)
+                this.timer = null
             }
 
             this.timer = setTimeout(() => {
-                this.loadLazyData();
-            }, 200);
+                const newUrl = this.route(`admin.${this.currentResource}.index`, this.lazyParams)
+                history.pushState(cloneDeep(this.lazyParams), null, newUrl)
+
+                this.loadLazyData()
+            }, 200)
         },
         loadLazyData() {
-            this.loading = true;
-            if (this.filter_begin_time) {
-                this.lazyParams.filter_begin_time = this.filter_begin_time;
-            }
-            if (this.filter_end_time) {
-                this.lazyParams.filter_end_time = this.filter_end_time;
-            }
+            this.loading = true
 
-            this.lazyParams.filters = this.getFilters();
-
-            this.$axios
-                .get(
-                    this.route(`admin.${this.currentResource}.index`, {
-                        ...this.lazyParams,
-                        page: this.lazyParams.page + 1,
-                    })
-                )
-                .then((res) => {
-                    const data = res.data;
-                    this.loading = false;
-                    this.items = data.data;
-                    this.rows = data.per_page;
-                    this.totalItems = data.total;
-                });
+            this.$axios.get(this.route(`admin.${this.currentResource}.index`, this.lazyParams)).then((res) => {
+                const data = res.data
+                this.loading = false
+                this.items = data.data
+                this.rows = data.per_page
+                this.totalItems = data.total
+            })
         },
         onPage(event) {
-            this.lazyParams = event;
-            this.loadLazyData();
+            this.lazyParams.page = event.page + 1
         },
-        onSort(event) {
-            this.lazyParams = event;
-            this.loadLazyData();
-        },
-        onFilter() {
-            this.lazyParams.filters = this.filters;
-            this.loadLazyData();
-        },
+        onSort(event) {},
+        onFilter(event) {},
         onSelectAllChange(event) {
-            const selectAll = event.checked;
+            const selectAll = event.checked
 
             if (selectAll) {
                 this.$axios
@@ -324,141 +274,127 @@ export default {
                         })
                     )
                     .then((res) => {
-                        this.selectAll = true;
-                        this.selectedItems = res.data.data;
-                    });
+                        this.selectAll = true
+                        this.selectedItems = res.data.data
+                    })
             } else {
-                this.selectAll = false;
-                this.selectedItems = [];
+                this.selectAll = false
+                this.selectedItems = []
             }
         },
         onRowSelect() {
-            this.selectAll = this.selectedItems.length === this.totalItems;
+            this.selectAll = this.selectedItems.length === this.totalItems
         },
         onRowUnselect() {
-            this.selectAll = false;
+            this.selectAll = false
         },
         mergeColumns() {
-            let schemaColumns = { ...this.schema.columns };
+            let schemaColumns = { ...this.schema.columns }
 
-            const columns = this.columns.length
-                ? this.columns
-                : this.pluck(Object.values(this.schema.columns), "label");
+            const columns = this.columns.length ? this.columns : this.pluck(Object.values(this.schema.columns), 'label')
 
             columns.forEach(function (column, index) {
-                let transformColumn = {};
+                let transformColumn = {}
 
-                if (typeof column === "object") {
+                if (typeof column === 'object') {
                     transformColumn = {
                         display: true,
                         order: index,
                         ...column,
-                    };
+                    }
                 } else {
                     transformColumn = {
                         display: true,
                         order: index,
                         field: column,
-                    };
+                    }
                 }
 
                 if (schemaColumns[transformColumn.field]) {
                     schemaColumns[transformColumn.field] = {
                         ...schemaColumns[transformColumn.field],
                         ...transformColumn,
-                    };
+                    }
                 } else {
                     schemaColumns[transformColumn.field] = {
-                        type: "text",
+                        type: 'text',
                         default: null,
                         label: transformColumn.field,
                         ...transformColumn,
-                    };
+                    }
                 }
-            });
+            })
 
-            schemaColumns["id"].order = 0;
+            schemaColumns['id'].order = 0
 
-            return schemaColumns;
+            return schemaColumns
         },
 
         getFilters() {
-            let keyword = route().params.global?.value;
+            let keyword = route().params.global?.value
             let filters = {
                 ...route().params,
-            };
+            }
 
             filters.global = {
                 value: keyword,
                 matchMode: FilterMatchMode.CONTAINS,
-            };
+            }
 
-            const columns = this.mergeColumns;
+            const columns = this.mergeColumns
 
             Object.keys(columns)
-                .filter(
-                    (x) => x === "id" || columns[x]?.rules?.includes("required")
-                )
+                .filter((x) => x === 'id' || columns[x]?.rules?.includes('required'))
                 .forEach((column) => {
                     filters[column] = {
                         value: null,
                         matchMode: FilterMatchMode.CONTAINS,
-                    };
-                });
+                    }
+                })
 
-            return filters;
+            return filters
         },
         getStyles(data, column) {
-            const value = data[column.field];
-            if (!column.list) return false;
+            const value = data[column.field]
+            if (!column.list) return false
 
-            return column.list.find((x) => x.label === value || x.id === value)
-                .styles;
+            return column.list.find((x) => x.label === value || x.id === value).styles
         },
         transformCell(data, column) {
-            let value = data[column.field];
+            let value = data[column.field]
 
             if (column.list) {
-                value = column.list.find(
-                    (x) => x.label === value || x.id === value
-                ).label;
+                value = column.list.find((x) => x.label === value || x.id === value).label
             }
 
             if (this.mergedColumns[column.field].transform) {
-                value = this.mergedColumns[column.field].transform(data);
-            } else if (column.type === "date") {
-                value = this.toDate(value, "DD/MM/YYYY");
-            } else if (column.type === "datetime") {
-                value = this.toDate(value);
-            } else if (column.type === "decimal") {
-                value = this.toMoney(value);
-            } else if (
-                (column.type === "bigint" || column.type === "integer") &&
-                column.field !== "id"
-            ) {
-                value = this.toNumber(value);
-            } else if (column.type === "boolean") {
+                value = this.mergedColumns[column.field].transform(data)
+            } else if (column.type === 'date') {
+                value = this.toDate(value, 'DD/MM/YYYY')
+            } else if (column.type === 'datetime') {
+                value = this.toDate(value)
+            } else if (column.type === 'decimal') {
+                value = this.toMoney(value)
+            } else if ((column.type === 'bigint' || column.type === 'integer') && column.field !== 'id') {
+                value = this.toNumber(value)
+            } else if (column.type === 'boolean') {
                 value = `<div class="flex m-auto w-3 h-3 ${
-                    value ? "bg-green-500" : "bg-orange-300"
-                } rounded-full"></div>`;
-            } else if (
-                column.type === "json" &&
-                value &&
-                Object.keys(value).length === 0
-            ) {
-                return null;
+                    value ? 'bg-green-500' : 'bg-orange-300'
+                } rounded-full"></div>`
+            } else if (column.type === 'json' && value && Object.keys(value).length === 0) {
+                return null
             }
 
-            return value;
+            return value
         },
         isImageCell(data, column) {
-            return this.isImage(data[column.field]?.path);
+            return this.isImage(data[column.field]?.path)
         },
         capitalize(string) {
-            return string?.charAt(0).toUpperCase() + string.slice(1);
+            return string?.charAt(0).toUpperCase() + string.slice(1)
         },
     },
-};
+}
 </script>
 
 <style lang="scss" scoped>
