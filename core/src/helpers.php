78eab23b757c50318731c80e8bae9e019be06e1c
Whoosh <nhatkha1407@gmail.com>
2023-02-20 15:25:11 +0700

    Update helpers.php

--- core/src/helpers.php
+++ core/src/helpers.php
@@ -254,3 +254,10 @@ if (!function_exists('clear_cache')) {
         return true;
     }
 }
+
+if (!function_exists('utf8_for_xml')) {
+    function utf8_for_xml($string)
+    {
+        return preg_replace('/[^\x{0009}\x{000a}\x{000d}\x{0020}-\x{D7FF}\x{E000}-\x{FFFD}]+/u', ' ', $string);
+    }
+}
