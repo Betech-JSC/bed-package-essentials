26b60fd75c900d1d1781540245ae39ef286c513b
Whoosh <nhatkha1407@gmail.com>
2023-02-23 23:36:51 +0700

    Update Setting.php

--- core/src/Models/Setting.php
+++ core/src/Models/Setting.php
@@ -53,4 +53,15 @@ class Setting extends Model
             ]
         ][$id] ?? [];
     }
+
+    public static function publicCustomVariables()
+    {
+        return cache_response('public_custom_vars', function () {
+            $settings = settings()->group('custom_vars')->all()->first();
+            $settings = collect(json_decode($settings, true))->filter(fn ($item) => $item
+            ['is_public']);
+
+            return $settings;
+        }, 'settings');
+    }
 }
