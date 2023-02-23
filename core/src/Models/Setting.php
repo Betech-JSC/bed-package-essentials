a05989ad36bc5abaf79c2ec4b26db2c71b5ba9b3
Whoosh <nhatkha1407@gmail.com>
2023-02-24 00:40:53 +0700

    Update Setting.php

--- core/src/Models/Setting.php
+++ core/src/Models/Setting.php
@@ -56,7 +56,7 @@ class Setting extends Model
 
     public static function customVariables($isPublic = null)
     {
-        $settings = cache_response('public_custom_vars', function () {
+        $settings = cache_response('custom_vars', function () {
             $settings = settings()->group('custom_vars')->all()->first();
             $settings = collect(json_decode($settings, true));
 
