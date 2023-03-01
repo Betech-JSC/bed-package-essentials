370f520f175304e0344e47cc78c927c8451a96da
Whoosh <nhatkha1407@gmail.com>
2023-03-01 15:57:45 +0700

    Update InputUpload.vue

--- core/resources/Backend/js/Components/Form/InputUpload.vue
+++ core/resources/Backend/js/Components/Form/InputUpload.vue
@@ -2,7 +2,7 @@
     <template v-if="multiple">
         <div class="grid gap-3" :class="itemsPerRow">
             <div
-                class="relative col-span-1 group"
+                class="relative flex items-center col-span-1 overflow-hidden rounded-sm bg-gray-50 group"
                 v-for="(file, index) in files"
                 :key="index"
             >
@@ -10,37 +10,9 @@
                     class="absolute inset-0 transition-opacity duration-200 bg-white opacity-0 group-hover group-hover:opacity-50"
                 ></div>
 
-                <div
-                    class="flex items-center justify-center overflow-hidden transition-colors duration-200 bg-gray-100 border border-gray-100 rounded select-none aspect-[1/1]"
-                >
-                    <img
-                        v-if="isImage(file.path)"
-                        :src="`${staticUrl(file.path)}?w=500`"
-                        class="object-contain w-full"
-                    />
-                    <div v-else class="flex items-center p-4 text-xs break-all">
-                        {{ file.filename }}
-                    </div>
-                </div>
-                <div
-                    class="absolute flex invisible space-x-1 transition-all duration-200 opacity-0 right-2 bottom-2 group-hover:opacity-100 group-hover:visible"
-                >
-                    <Button
-                        @click="chosenImage(file)"
-                        label="Chọn"
-                        class="btn-sm btn-white"
-                    />
-                    <Button
-                        @click="removeSelectedFiles(index)"
-                        label="Xóa"
-                        class="btn-sm btn-white"
-                    />
-                </div>
+                <Thumbnail :file="file" @remove="removeSelectedFiles(index)" />
             </div>
-            <div
-                class="relative col-span-1 group"
-                v-if="files.length < maxItems && canAddFile"
-            >
+            <div class="relative col-span-1 group" v-if="files.length < maxItems && canAddFile">
                 <div
                     class="overflow-hidden text-gray-400 transition-colors duration-200 border border-gray-200 border-dashed rounded cursor-pointer select-none hover:border-gray-400 hover:text-gray-600 aspect-[1/1]"
                     @click="showMediaManager = true"
@@ -54,55 +26,24 @@
     </template>
     <template v-else>
         <template v-if="Array.isArray(files) && files.length > 0">
-            <div
-                class="relative flex h-full max-w-xs bg-gray-200 border rounded-sm group"
-            >
-                <div class="absolute inset-0 flex items-center justify-center">
-                    <div
-                        class="absolute inset-0 transition-opacity duration-200 bg-white opacity-0 group-hover group-hover:opacity-50"
-                        @click="showMediaManager = true"
-                    ></div>
-                    <div
-                        class="absolute flex invisible space-x-2 transition-all duration-200 opacity-0 right-2 bottom-2 group-hover:opacity-100 group-hover:visible"
-                    >
-                        <Button
-                            size="xs"
-                            @click="removeSelectedFiles"
-                            label="Xóa"
-                            class="btn-sm btn-white"
-                        />
-                    </div>
-                </div>
-                <img
-                    v-if="isImage(files[0].path)"
-                    :src="`${staticUrl(files[0].path)}?w=500`"
-                    class="object-contain w-full"
-                />
-                <div v-else class="flex items-center p-4 text-xs break-all">
-                    {{ files[0].filename }}
-                </div>
+            <div class="relative flex items-center h-full max-w-xs overflow-hidden border rounded-sm bg-gray-50 group">
+                <Thumbnail :file="files[0]" @remove="removeSelectedFiles" />
             </div>
         </template>
         <template v-else>
             <div
-                class="relative w-full h-full max-w-xs p-3 text-gray-700 transition-colors duration-200 bg-gray-100 rounded-sm cursor-pointer select-none hover:bg-gray-200 hover:text-gray-900"
+                class="relative w-full h-full max-w-xs p-3 text-gray-700 transition-colors duration-200 rounded-sm cursor-pointer select-none bg-gray-50 hover:bg-gray-200 hover:text-gray-900"
                 @click="showMediaManager = true"
             >
                 <div
                     class="flex flex-col items-center justify-center w-full h-full py-4 space-y-2 text-xs font-medium text-center text-gray-600 border border-gray-400 border-dashed"
                 >
-                    <!-- <Icon name="image" v-if="field.icon ?? true" /> -->
                     <span>CLICK ĐỂ CHỌN</span>
                 </div>
             </div>
         </template>
     </template>
-    <FileManager
-        v-if="showMediaManager"
-        v-model:show="showMediaManager"
-        @onSelect="onSelect"
-        :multiple="multiple"
-    />
+    <FileManager v-if="showMediaManager" v-model:show="showMediaManager" @onSelect="onSelect" :multiple="multiple" />
 
     <Dialog
         header="Folder"
@@ -116,11 +57,7 @@
     >
         <div class="space-y-6">
             <div>
-                <label
-                    class="block mb-2 font-semibold tracking-wide text-gray-700 font-display"
-                >
-                    URL
-                </label>
+                <label class="block mb-2 font-semibold tracking-wide text-gray-700 font-display"> URL </label>
                 <input
                     v-model="showFileModal.link"
                     type="text"
@@ -130,9 +67,7 @@
             <template v-if="showFileModal.options">
                 <template v-for="(option, field) in showFileModal.options">
                     <div>
-                        <label
-                            class="block mb-2 font-semibold tracking-wide text-gray-700 font-display"
-                        >
+                        <label class="block mb-2 font-semibold tracking-wide text-gray-700 font-display">
                             {{ field }}
                         </label>
                         <input
@@ -147,76 +82,70 @@
 
         <template #footer>
             <Button variant="white" @click="showFileModal = null" label="Hủy" />
-            <Button
-                type="button"
-                class="ml-2"
-                @click="editFileUpdate"
-                label="Lưu"
-            />
+            <Button type="button" class="ml-2" @click="editFileUpdate" label="Lưu" />
         </template>
     </Dialog>
 </template>
 
 <script>
-import FileManager from "@Core/Components/FileManager.vue";
+import FileManager from '@Core/Components/FileManager.vue'
+import Thumbnail from '@Core/Components/Thumbnail.vue'
 
 export default {
     components: {
         FileManager,
+        Thumbnail,
     },
 
-    props: ["field", "modelValue"],
-    emits: ["change"],
+    props: ['field', 'modelValue'],
+    emits: ['change'],
 
     data() {
         return {
             files: [],
             showMediaManager: false,
             showFileModal: null,
-        };
+        }
     },
 
     computed: {
         urlOnly() {
-            return this.field?.urlOnly ?? false;
+            return this.field?.urlOnly ?? false
         },
         multiple() {
-            return this.field?.multiple ?? false;
+            return this.field?.multiple ?? false
         },
         accept() {
-            return (
-                this.field.accept ??
-                "image/png, image/gif, image/jpeg ,image/webp"
-            );
+            return this.field.accept ?? 'image/png, image/gif, image/jpeg ,image/webp'
         },
         itemsPerRow() {
-            return this.field.perRow ?? "grid-cols-4";
+            return this.field.perRow ?? 'grid-cols-4'
         },
         maxItems() {
-            return this.field.max || 99;
+            return this.field.max || 99
         },
         expectedUrl() {
-            return this.field.expected ?? false;
+            return this.field.expected ?? false
         },
         canAddFile() {
-            return this.field.canAddFile ?? true;
+            return this.field.canAddFile ?? true
         },
     },
 
     watch: {
         modelValue() {
-            this.files = [];
-            this.initFiles();
+            this.files = []
+            this.initFiles()
         },
     },
 
     created() {
-        this.initFiles();
+        this.initFiles()
     },
 
     methods: {
         chosenImage(file) {
-            this.$bus.emit("SelectedImage", file);
+            this.$bus.emit('SelectedImage', file)
         },
         initFiles() {
             if (this.urlOnly) {
@@ -224,67 +153,63 @@ export default {
                     this.files = this.modelValue?.map(function (url) {
                         return {
                             static_url: url,
-                        };
-                    });
+                        }
+                    })
                 } else if (
                     !this.multiple &&
                     this.modelValue !== null &&
                     this.modelValue !== undefined &&
                     Object.keys(this.modelValue).length
                 ) {
-                    this.files = [{ static_url: this.modelValue }];
+                    this.files = [{ static_url: this.modelValue }]
                 }
             } else {
                 if (this.multiple) {
-                    this.files = this.modelValue;
+                    this.files = this.modelValue
                 } else if (
                     !this.multiple &&
                     this.modelValue !== null &&
                     this.modelValue !== undefined &&
                     Object.keys(this.modelValue).length
                 ) {
-                    this.files = [this.modelValue];
+                    this.files = [this.modelValue]
                 }
             }
         },
         onSelect(files) {
             if (this.multiple) {
-                const selectedFiles = this.pluck(this.files, "path");
-                const diffFiles = files.filter(
-                    (x) => !selectedFiles.includes(x.path)
-                );
+                const selectedFiles = this.pluck(this.files, 'path')
+                const diffFiles = files.filter((x) => !selectedFiles.includes(x.path))
 
-                this.files = this.files.concat(diffFiles);
-                this.files = this.files.slice(0, this.maxItems);
+                this.files = this.files.concat(diffFiles)
+                this.files = this.files.slice(0, this.maxItems)
             } else {
-                this.files = files;
+                this.files = files
             }
             if (this.urlOnly) {
                 if (this.multiple) {
-                    this.$emit("change", pluck(this.files, "static_url"));
+                    this.$emit('change', pluck(this.files, 'static_url'))
                 } else {
-                    this.$emit("change", this.files[0]?.static_url);
+                    this.$emit('change', this.files[0]?.static_url)
                 }
             } else {
                 if (this.multiple) {
-                    this.$emit("change", this.files);
+                    this.$emit('change', this.files)
                 } else {
-                    this.$emit("change", this.files[0]);
+                    this.$emit('change', this.files[0])
                 }
             }
         },
         editFileUpdate() {
-            const index = this.files.findIndex(
-                (x) => x.url === this.showFileModal.url
-            );
-            this.files[index] = this.showFileModal;
-            this.showFileModal = null;
-            this.$emit("change", this.files);
+            const index = this.files.findIndex((x) => x.url === this.showFileModal.url)
+            this.files[index] = this.showFileModal
+            this.showFileModal = null
+            this.$emit('change', this.files)
         },
         removeSelectedFiles(index = 0) {
-            this.files.splice(index, 1);
-            this.$emit("change", this.files);
+            this.files.splice(index, 1)
+            this.$emit('change', this.files)
         },
     },
-};
+}
 </script>
