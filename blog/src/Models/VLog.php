2a0be206db45fa9a52e6d55276d72bf7a3e5a560
LinhPhan9605 <71279918+LinhPhan9605@users.noreply.github.com>
2023-03-20 09:00:56 +0700

    project

--- blog/src/Models/VLog.php
+++ blog/src/Models/VLog.php
@@ -31,6 +31,7 @@ class Vlog extends BaseModel
         'position',
         'view_count',
         'image',
+        'time',
 
         'inject_head',
         'inject_body_start',
@@ -155,7 +156,8 @@ class Vlog extends BaseModel
             'published_at' => $this->published_at,
             'description' => $this->description,
             'image' => $this->getImageDetail($this->image),
-            'url' => $this->current_url
+            'url' => $this->current_url,
+            'time' =>$this->time,
         ];
 
         if (isset($conditions['tags']) && $conditions['tags']) {
@@ -175,7 +177,8 @@ class Vlog extends BaseModel
             'image' => $this->getImageDetail($this->image),
             'video' => $this->getImageDetail($this->video),
             'video_url' => $this->video_url,
-            'url' => $this->current_url
+            'url' => $this->current_url,
+            'time' =>$this->time,
         ];
 
         if (isset($conditions['tags']) && $conditions['tags']) {
