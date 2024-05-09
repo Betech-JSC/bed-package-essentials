a4fcadd2ae680b0784abcb3f7807901c87687522
toannguyen112 <nguyencongtoana1nhvt@gmail.com>
2024-05-09 15:37:06 +0700

    Update Guest.vue

--- core/resources/Backend/js/Layouts/Guest.vue
+++ core/resources/Backend/js/Layouts/Guest.vue
@@ -2,10 +2,6 @@
     <div
         class="flex flex-col items-center min-h-screen bg-gray-100 sm:justify-center"
     >
-        <div
-            class="w-full max-w-md px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:rounded-lg"
-        >
-            <slot />
-        </div>
+        <slot />
     </div>
 </template>
